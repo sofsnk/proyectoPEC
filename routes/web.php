9fc8eb9bd32fa6ff2f7ed4293168ede64b8e319e
oscxr <oruiz5761@gmail.com>
2025-06-11 14:15:22 -0600

    se corrigieron las vistas mostradas

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 //Pagina principal
 Route::view('/', 'index')->name('home');
 Route::view('/centros de acopio', 'centros_acopio')->name('centros');
-Route::view('/estadisticas', 'centros_acopio')->name('estadisticas');
-Route::view('/galeria de imagenes', 'centros_acopio')->name('galeria');
+Route::view('/estadisticas', 'estadisticas')->name('estadisticas');
+Route::view('/galeria de imagenes', 'galeria')->name('galeria');
 
 //Donar dispositivos
 Route::view('/donar dispositivo', 'donacion.donar')->name('donacion.donar');
@@ -18,10 +18,10 @@ Route::view('/donaciones/historial', 'donacion.historial')->name('donacion.histo
 Route::view('/admin/login', 'admin.auth.login')->name('admin.login');
 Route::view('/admin/register', 'admin.auth.register')->name('admin.register');
 
-Route::middleware(AuthAdmin::class)->group(function () {
-    Route::get('/admin', [AdminController::class, 'inicio'])->name('admin.inicio');
-    Route::get('/admin/dispositivos', [AdminController::class, 'dispositivos'])->name('admin.dispositivos');
-    Route::get('/admin/donaciones', [AdminController::class, 'donaciones'])->name('admin.donaciones');
-    Route::get('/admin/estadisticas', [AdminController::class, 'estadisticas'])->name('admin.estadisticas');
-    Route::get('/admin/ventas', [AdminController::class, 'ventas'])->name('admin.ventas');
+Route::prefix('/admin')->middleware(AuthAdmin::class)->group(function () {
+    Route::get('/', [AdminController::class, 'inicio'])->name('admin.inicio');
+    Route::get('/dispositivos', [AdminController::class, 'dispositivos'])->name('admin.dispositivos');
+    Route::get('/donaciones', [AdminController::class, 'donaciones'])->name('admin.donaciones');
+    Route::get('/estadisticas', [AdminController::class, 'estadisticas'])->name('admin.estadisticas');
+    Route::get('/ventas', [AdminController::class, 'ventas'])->name('admin.ventas');
 });
