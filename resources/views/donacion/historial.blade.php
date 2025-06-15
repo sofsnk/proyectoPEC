6463887169fcc458be98dc4050f88a8d422e8243
oscxr <oruiz5761@gmail.com>
2025-06-14 22:26:12 -0600

    .

--- resources/views/donacion/historial.blade.php
+++ resources/views/donacion/historial.blade.php
@@ -13,23 +13,27 @@
     @include('layouts.nav')
     <main>
         <h1>Historial de donaciones</h1>
+        @if (!empty($historial))
         @foreach ($historial as $donacion)
         <h3>donacion: </h3>
-            <p>fecha: {{ $donacion['fecha_donacion'] }}</p>
-            <p>estado: {{ $donacion['estado'] }}</p>
-            <p>cantidad de dispositivos: {{ $donacion['cantidad_dispositivos'] }}</p>
-            <h4>dispositivos: </h4>
-            @foreach ($donacion['dispositivos'] as $dispositivo)
-                <p>dispositivo: {{ $dispositivo['nombre_dispositivo'] }}</p>
-                <p>marca: {{ $dispositivo['marca'] }}</p>
-                <p>modelo: {{ $dispositivo['modelo'] }}</p>
-                <p>estado: {{ $dispositivo['estado_fisico'] }}</p>
-                <p>categoria: {{ $dispositivo['categoria']['nombre_categoria'] }}</p>
-                <br>
-            @endforeach
-            <br>
+        <p>fecha: {{ $donacion['fecha_donacion'] }}</p>
+        <p>estado: {{ $donacion['estado'] }}</p>
+        <p>cantidad de dispositivos: {{ $donacion['cantidad_dispositivos'] }}</p>
+        <h4>dispositivos: </h4>
+        @foreach ($donacion['dispositivos'] as $dispositivo)
+        <p>dispositivo: {{ $dispositivo['nombre_dispositivo'] }}</p>
+        <p>marca: {{ $dispositivo['marca'] }}</p>
+        <p>modelo: {{ $dispositivo['modelo'] }}</p>
+        <p>estado: {{ $dispositivo['estado_fisico'] }}</p>
+        <p>categoria: {{ $dispositivo['categoria']['nombre_categoria'] }}</p>
+        <br>
         @endforeach
-    </main>
+        <br>
+        @endforeach
+        @else
+        <p>Aún no ha hecho ninguna donación</p>
+        @endif
+        </main>
     @include('layouts.footer')
 </body>
 
