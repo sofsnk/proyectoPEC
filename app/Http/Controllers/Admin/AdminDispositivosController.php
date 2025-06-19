780899bd63f794cafbf94d0f12c30649cba14cf5
Sofia Villanueva <sofiavivas8002@gmail.com>
2025-06-19 13:11:54 -0600

    Update AdminDispositivosController.php

--- app/Http/Controllers/Admin/AdminDispositivosController.php
+++ app/Http/Controllers/Admin/AdminDispositivosController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Dispositivo;
 use Illuminate\Http\Request;
 
 class AdminDispositivosController extends Controller
 {
-    //
+    public function index()
+    {
+        $dispositivosData = Dispositivo::all();
+        $dispositivos = $dispositivosData->toArray();
+
+        return view('admin.dispositivos', compact('dispositivos'));
+    }
 }
